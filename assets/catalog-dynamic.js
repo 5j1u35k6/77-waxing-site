0e19f0714833d14dc1fa639041adf4493f0938c3
5j1u35k6 <max19450@gmail.com>
2026-09-09 21:33:08 +0800

    refresh catalog store dependency

--- assets/catalog-dynamic.js
+++ assets/catalog-dynamic.js
@@ -1,4 +1,4 @@
-import { loadCatalog, publicCatalog, watchCatalog } from "./service-catalog-store.js?v=20260909-2035";
+import { loadCatalog, publicCatalog, watchCatalog } from "./service-catalog-store.js?v=20260909-2129";
 
 const B = "/77-waxing-site";
 let catalog = [];
